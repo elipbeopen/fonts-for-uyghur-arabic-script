4356c94ab9c8be2a84019989b94544ffc8668682
babbage <601728820@qq.com>
2020-04-25 11:38:59 +0800

    feat: 所有字体重新按照新的命名规范起名,fontlist.json中补充了字体的nameing_table对象信息

--- ttf_convert2_woff2/conver.js
+++ ttf_convert2_woff2/conver.js
@@ -2,21 +2,38 @@ const fs = require("fs");
 const { join } = require("path");
 
 const ttf2woff2 = require("ttf2woff2");
+const opentype = require("opentype.js");
+
+function camelCase(words) {
+  var pattern = /[A-Z]|\s+[A-z]|\s+[0-9]/g;
+  words = words.replace(/_|-/g, " ");
+  return lcFirst(words.replace(pattern, (match) => match.trim().toUpperCase()));
+}
+function ucFirst(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function lcFirst(string) {
+  return string.charAt(0).toLowerCase() + string.slice(1);
+}
+
+function studlyCase(selectedWords) {
+  return ucFirst(camelCase(selectedWords));
+}
 
 //获取字体列表
 const getFonts = (fontsPath) => {
   return fs.readdirSync(fontsPath).map((font) => {
     return {
       font,
-      fontName: font.split(".").shift().replace(/\s+/g, "").trim(),
       fontPath: fontsPath,
     };
   });
 };
 
 //ttf conver 2 woff2
-const convert = ({ font, fontName, fontPath }) => {
-  console.log(`convert ${fontName} ...`);
+const convert = ({ font, fontPath }) => {
+  console.log(`convert ${font} ...`);
   return ttf2woff2(fs.readFileSync(join(fontPath, font)));
 };
 
@@ -25,26 +42,35 @@ const finishWork = (fontsPath) => {
   let fontList = {};
   //遍历所有字体
   getFonts(fontsPath).forEach((fontInfo) => {
+    let ttf_orign = join(fontInfo.fontPath, fontInfo.font);
+
+    let opentypeFontInfo = opentype.loadSync(ttf_orign).names;
+
+    let fontFamilyName = opentypeFontInfo.fontFamily.en;
+    let fontSubfamilyEn = opentypeFontInfo.fontSubfamily.en;
+    let fontTTFName = studlyCase(fontFamilyName) + "_" + fontSubfamilyEn;
+
     // //生成目录
-    let outPath = join(fontInfo.fontPath, "../assets/fonts", fontInfo.fontName);
+    let outPath = join(fontInfo.fontPath, "../assets/fonts", fontTTFName);
     if (!fs.existsSync(outPath)) {
       fs.mkdirSync(outPath, { recursive: true });
     }
 
     //生成woff2并写入文件
     fs.writeFileSync(
-      join(outPath, `./${fontInfo.fontName}.woff2`),
+      join(outPath, `./${fontTTFName}.woff2`),
       convert(fontInfo)
     );
     //吧字体也复制放在一起
     fs.copyFileSync(
       join(fontInfo.fontPath, fontInfo.font),
-      join(outPath, `${fontInfo.fontName}.ttf`)
+      join(outPath, `${fontTTFName}.ttf`)
     );
 
-    fontList[fontInfo.fontName] = {
-      ttf: `./fonts/${fontInfo.fontName}/${fontInfo.fontName}.ttf`,
-      woff2: `./fonts/${fontInfo.fontName}/${fontInfo.fontName}.woff2`,
+    fontList[fontTTFName] = {
+      ttf: `fonts/${fontTTFName}/${fontTTFName}.ttf`,
+      woff2: `fonts/${fontTTFName}/${fontTTFName}.woff2`,
+      nameing_table: opentypeFontInfo,
     };
   });
 
